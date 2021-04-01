407aec372432e515de7c401f9e4e45122e086e56
Long Duong <long66704@gmail.com>
2021-04-01 21:17:34 +0700

    Clean and add scss

---
+++ src/components/sign-in/sign-in.jsx
@@ -0,0 +1,14 @@
+import './sign-in.scss';
+import React from 'react';
+
+export const SignIn = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+
+
---
+++ src/components/sign-up/sign-up.jsx
@@ -0,0 +1,13 @@
+import './sign-up.scss';
+
+
+export const SignUp = ({}) => {
+    return (
+        <div className='sign-up-67h'>
+            <div className='text'>
+                asdfg
+            </div>
+            
+        </div>
+    )
+}
