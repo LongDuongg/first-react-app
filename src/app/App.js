1c3792582224e392e3aca641ede8b65bae469ae2
luan duong <luan.dph@gmail.com>
2021-04-03 02:23:22 +0700

    routing

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,7 @@
 import './App.scss';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import {Route1} from './routes/route-1/route-1';
-import {Route2} from './routes/route-2/route-2';
+import {Route1} from './auth-routes/route-1/route-1';
+import {Route2} from './auth-routes/route-2/route-2';
 
 function App({}) {
   return (
@@ -17,6 +17,7 @@ const Routes = () => {
   return (
     <Router>
       <Switch>
+        <Route exact path='/' component={Route1} />
         <Route exact path='/route-1' component={Route1}/>
         <Route exact path='/route-2' component={Route2}/>
       </Switch>
